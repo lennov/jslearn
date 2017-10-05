fce3e5f778050fa6214a778e30ddad1f6321f050
Lennov Roman <rlennov@alfabank.ru>
2017-10-05 17:07:28 +0300

    1st done

--- home1.js
+++ home1.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("homework 1");
+function parseAmount(strAmount){
+    if(strAmount == null){
+        return NaN;
+    }
+    return parseFloat(strAmount.replace(",", ".")) * 100;
+}
+
+function runParseAmount(amount){
+    let parsed = parseAmount(amount)
+    console.log("input: '%s' \tvalue: %d(%s)", amount, parsed, typeof(parsed));
+}
+
+runParseAmount('10'); // -> 1000
+runParseAmount('10,5'); // -> 1050
+runParseAmount('10,55'); // -> 1055
+runParseAmount('d55');
+runParseAmount('');
